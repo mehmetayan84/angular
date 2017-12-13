b4374d4b5bcede7b5f44614e0ed81c8ee2366dad
Ayan <mehmet.ayan@siemens.com>
2017-12-13 20:59:05 +0300

    interpolation and property binding is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
+  styles: ['.blue-bg{background-color:blue;}']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  
+  private currentUser:User = {username: "Test", email: "test@test.com"};
+  
+  private hideEmail:boolean = true;
+
+  private disabledInput:boolean = false;
+
+  private isDisabled():boolean{
+    return this.disabledInput;
+  }
+
+  private isBlueBg:boolean = true;
+  private bgColor:string = "red";
+}
+
+interface User{
+  username:string;
+  email:string;
+}
