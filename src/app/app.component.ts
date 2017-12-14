4238797a5b897af53db6d083561466f27b622eda
MehmetAyan <mehmetayan84@gmail.com>
2017-12-15 00:52:25 +0300

    creating own application lesson is reached

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,15 +17,31 @@ export class AppComponent  {
     this.hideEmail = !this.hideEmail;
   }
 
-  private isDisabled():boolean{
+  private changeMail(newMail:string){
+    this.currentUser.email = newMail;
+  }
+
+  private keyPress(event: KeyboardEvent){
+    if (event.keyCode == 13){
+      this.changeMail((<HTMLInputElement>event.target).value);
+    }
+  }
+
+  private isDisabled(): boolean{
     return this.disabledInput;
   }
 
-  private isBlueBg:boolean = true;
-  private bgColor:string = "red";
+  private isBlueBg: boolean = true;
+  private bgColor: string = "red";
+
+  private users: Array<User>= [
+    {username: "user1", email: "user1@test.com"},
+    {username: "user2", email: "user2@test.com"},
+    {username: "user3", email: "user3@test.com"},
+  ]
 }
 
 interface User{
-  username:string;
-  email:string;
+  username: string;
+  email: string;
 }
