b4634fbb4b7446d5bc6370632bff7f166fcb2f14
MehmetAyan <mehmet.ayan@siemens.com>
2017-12-22 09:21:25 +0300

    ngmodule file is updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import {Account} from './account/account.model';
-import {AccountsList} from './account/accounts_list.components';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'my-app',
@@ -10,12 +8,6 @@ import { BrowserModule } from '@angular/platform-browser';
   styleUrls: ['app/app.component.css']
 })
 
-@NgModule({
-  imports:[BrowserModule],
-  declarations:[AppComponent, AccountsList, Account],
-  bootstrap: [AppComponent],
-  providers:[]
-})
 
 export class AppComponent  {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AccountsList} from './account/accounts_list.components';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, AccountsList],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
