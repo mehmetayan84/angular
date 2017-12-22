351b9865ab02d45c3a41ff4e67f1c70702dfef1e
MehmetAyan <mehmet.ayan@siemens.com>
2017-12-22 16:36:22 +0300

    input decorator is added to project

--- src/app/account/accounts_list.components.ts
+++ src/app/account/accounts_list.components.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 import {Account} from './account.model';
 
@@ -9,42 +9,11 @@ import {Account} from './account.model';
 })
 
 export class AccountsList{
-    
-    private _selected:Array<boolean> = [false, false];
 
-     private _accounts:Array<Account> = [
-        {id:1, title:"Bank Xyz", description: "This is my main bank account", balance:501.2},
-        {id:2, title:"Bank Asd", description:"My secret account", balance:1024.10}
-    ]
+    @Input('accounts') _accounts:Array<Account>;
 
-    private _maxId = this.findMaxId();
-
-    private findMaxId()
-    {
-        let max= 0;
-        if(this._accounts && this._accounts.length > 0)
-        {
-             max = this._accounts[0].id;
-        }
-
-        for(let account of this._accounts){
-            if(account.id > max)
-             {
-                 max = account.id;
-             }
-         }
-
-        return max;
-  }
-
-  private removeAccount(index:number){
-    this._accounts.splice(index, 1);
-    this._selected.splice(index, 1);
-    this._maxId = this.findMaxId();
-  }
-
-  private select(index:number){
-    this._selected[index] = !this._selected[index];
-  }
+    private removeAccount(index:number){
+        this._accounts.splice(index, 1);
+    }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {Account} from './account/account.model';
 
-
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
@@ -11,16 +10,22 @@ import {Account} from './account/account.model';
 
 export class AppComponent  {
 
-  // private createAccount(titleEl: any, descEl: any, balEl:any){
-  //   alert("This method hasn't beern implemented yet");
-  //   // this._accounts.push(new Account(this._maxId+1, titleEl.value, descEl.value, balEl.value));
-  //   // this._selected.push(false);
-  //   // this._maxId = this.findMaxId();
+  private _accounts:Array<Account> = [
+    {id:1, title:"Bank Xyz", description: "This is my main bank account", balance:501.2},
+    {id:2, title:"Bank Asd", description:"My secret account", balance:1024.10}
+]
+  private _nextId = 3;
 
-  //   titleEl.value = "";
-  //   descEl.value= "";
-  //   balEl.value = 0;
-  // }
+  private createAccount(_nextId:number, titleEl: any, descEl: any, balEl:any){
+     alert("This method hasn't beern implemented yet");
+  //   // this._accounts.push(new Account(this._nextId, titleEl.value, descEl.value, balEl.value));
+  //   // this._selected.push(false);
+  //   // this._nextId = this._nextId++;
+
+     titleEl.value = "";
+     descEl.value= "";
+     balEl.value = 0;
+  }
 
 }
 
